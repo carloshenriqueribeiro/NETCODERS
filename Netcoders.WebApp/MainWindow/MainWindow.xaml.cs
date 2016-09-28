cbbaeadcf2556507e833d9cd5319db8974abb751
Carlos Henrique Oliveira Ribeiro <Carlos Henrique Oliveira Ribeiro>
2016-09-28 20:13:01 -0300

    first

---
+++ Netcoders.WebApp/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Business;
+using Model;
+using System.Windows;
+using MainWindow;
+
+
+namespace MainWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private ContatoBusiness _contatoBusiness;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var _contatoBusiness = new ContatoBusiness();
+            CarregaContatos();
+
+        }
+
+        public ContatoBusiness ContatoBusiness
+        {
+            get
+            {
+                return _contatoBusiness;
+            }
+
+            set
+            {
+                _contatoBusiness = value;
+            }
+        }
+
+        public void CarregaContatos()
+        {
+            try
+            {
+                dataGrid.ItemsSource = ContatoBusiness.GetAll();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public void CarregaSexo()
+        {
+            try
+            {
+                comboSexo.ItemsSource = _sexoBusiness.GetAll();
+                comboSexo.ItemsSource
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+
+
+
+
+        }
+        private void dataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+            var contato = (ContatoModel) dataGrid.SelectedItem;
+
+            txtNome.Text = contato.Nome;
+            txtEmail.txt = contato.Email;
+            txtTelefone.txt = contato.Telefone;
+            txtData.txt = contato.DataNascimento;
+            txtSexo.txt = contato.Sexo.Sexo;
+               
+
+        }
+    }
+    }
+
---
+++ Netcoders.WebApp/test/Test/ContatoTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+using System.Linq;
+
+namespace Test
+{
+    
+    [TestClass]
+    public class ContatoTest
+    {
+
+        [TestMethod]
+        public void ExisteContato()
+        {
+            var rep = new ContatoRepository();
+
+            var list = rep.GetAll();
+
+            // se a lista estiver vazia, lançar falhar
+            //if (!list.Any()) Assert.Fail();
+
+            var qtdeEsperada = 4;
+
+            Assert.AreEqual(qtdeEsperada, list.Count());
+
+            
+        }
+
+    }
+}
